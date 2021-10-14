2aea2622b661faedba27221c89c252893e95bee2
JoelMichaelRutter <joel-rutter@live.co.uk>
2021-10-14 19:10:42 +0000

    commiting for deployment

--- run.py
+++ run.py
@@ -1,9 +1,18 @@
+"""
+Import OS module
+"""
 import os
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request, flash
+if os.path.exists("env.py"):
+    import env
+# Must import request module from flask, import
+# flash module must be imported to show feedback
+# flash needs secret key
 
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
 
 
 @app.route("/")
@@ -29,8 +38,22 @@ def about():
 # to activate the links {{url_for('file name - no extension')}}
 
 
-@app.route("/contact")
+@app.route("/about/<member_name>")
+def about_member(member_name):
+    member = {}
+    with open("data/company.json", "r") as json_data:
+        data = json.load(json_data)
+        for obj in data:
+            if obj["url"] == member_name:
+                member = obj
+    return render_template("member.html", member=member)
+
+
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        flash("Thanks {}, we have received your message".format(
+            request.form.get("name")))
     return render_template("contact.html", page_title="Contact")
 
 
